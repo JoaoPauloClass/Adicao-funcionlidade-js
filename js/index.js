9bc1585c51a3379a768dcb28881dc523d9833dc5
JoaoPauloClass <theofficialspam8@gmail.com>
2023-11-12 17:36:51 -0300

    domingo remocao das partidas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
+const testAPIkey = "test_2f391f89e479a44134ad08f1357035";
+const prodAPIkey = "live_dab12fa5cbc0a77918ce519f226975";
+const APIkey = testAPIkey;
+
+
+function createErrorCard() {
+
+    //pegar a div principal dos campeonatos
+    const mainLeagues = document.querySelector(".main-leagues");
+
+    let errorLeague = document.createElement(`div`); //criar div para colocar o campeonato
+    errorLeague.classList.add("error-league");
+
+    const errorImg = document.createElement("img")
+    const errorMsg = document.createElement("p")
+
+    errorImg.src = '../imgs/x.png'
+    errorImg.alt = "Icone de erro"
+    errorMsg.textContent = "Não foi possível carregar os campeonatos"
+
+    errorLeague.appendChild(errorImg)
+    errorLeague.appendChild(errorMsg)
+
+    mainLeagues.appendChild(errorLeague)
+}
+
+
 function createLeagueCard(element) {
   //pegar a div principal dos campeonatos
   const mainLeagues = document.querySelector(".main-leagues");
@@ -38,123 +65,23 @@ function createLeagueCard(element) {
   mainLeagues.appendChild(league);
 }
 
-function createMatchCard(element) {
-  /*
-    // ---- CRIAR OS CARTÕES DE PARTIDAS ----
-    const todaysMatches = document.querySelector('.todays-matches-div')
-
-    //id dos campeonatos que estão acontecendo
-    incommingLeagues.forEach((element) => {
-      console.log(incommingLeagues);
-
-      //pegar as partidas que já aconteceram ou vão acontecer usando o id dos campeonatos que estão acontecendo
-      fetch(
-        `https://api.api-futebol.com.br/v1/campeonatos/${element}/partidas`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${APIkey}`,
-          },
-        }
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-
-          const fase = data.partidas.partidas["primeira-fase"];
-
-          //acessando as chaves
-          for (var key in fase) {
-            console.log(key);
-            let chave = fase[key];
-            console.log(chave);
-
-            const match = document.createElement("div");
-            match.classList.add("div-match");
-
-            const matchDiv1 = document.createElement("div");
-            const matchDiv2 = document.createElement("div");
-
-            match.appendChild(matchDiv1);
-            match.appendChild(matchDiv2);
-
-            //criar elementos da primeira div
-            const homeTeamLogo = document.createElement("img");
-
-            const homeTeamName = document.createElement("p");
-
-            const xSpan = document.createElement("span");
-
-            const visitorTeamName = document.createElement("p");
-
-            const visitorTeamLogo = document.createElement("img");
-
-            //criar elementos da segunda div
-            const matchDate = document.createElement("p");
-            const matchStatus = document.createElement("p");
-
-            homeTeamLogo.src = chave.ida.time_mandante.escudo;
-            homeTeamLogo.alt = "Logo do time mandante";
-
-            visitorTeamLogo.src = chave.ida.time_visitante.escudo;
-            visitorTeamLogo.alt = "Logo do time visitante";
-
-            leagueLogo.src = element.logo;
-            leagueLogo.alt = "Logo do campeonato";
-
-            leagueName.textContent = element.nome;
-            leagueYear.textContent = element.edicao_atual.temporada;
-            leaguePhase.textContent = element.fase_atual.nome;
-            leagueRound.textContent = element.rodada_atual
-              ? element.rodada_atual.nome
-              : null;
-
-            league.appendChild(leagueLogo);
-            league.appendChild(leagueName);
-            league.appendChild(leagueYear);
-            league.appendChild(leaguePhase);
-
-            todaysMatches.appendChild(match);
-          }
-        });
-
-      // ---------------------------- FILTRAR PARTIDAS ----------------------------
-
-      fetch("https://api.api-futebol.com.br/v1/ao-vivo", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${APIkey}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          const liveMatches = data;
-
-          console.log("filtrar");
-
-          console.log(liveMatches.placar);
-        });
-    });*/
-}
 
 function createCards(campeonatos) {
-  //array para colocar o id dos campeonatos que estão ocorrendo
-  var incommingLeagues = [];
 
   //verificar quais campeonatos estão ocorrendo
   campeonatos.forEach((element) => {
     if (element.status != "finalizado") {
       createLeagueCard(element); // criar cartão de campeonato
-      incommingLeagues.push(element.campeonato_id); // colocar o id do campeonato no array de campeonatos que estão ocorrendo
     }
   });
 }
 
 // ------------------- MAIN -------------------
 document.addEventListener("DOMContentLoaded", () => {
-  const testAPIkey = "test_2f391f89e479a44134ad08f1357035";
-  const prodAPIkey = "live_dab12fa5cbc0a77918ce519f226975";
-  const APIkey = testAPIkey;
+
+  const modal = document.querySelector('dialog')
+
+  modal.showModal()
 
   //consumir API
   fetch("https://api.api-futebol.com.br/v1/campeonatos", {
@@ -163,12 +90,18 @@ document.addEventListener("DOMContentLoaded", () => {
       Authorization: `Bearer ${APIkey}`, //passar na autenticação da API
     },
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json()
+    })
     .then((data) => {
-      console.log(data);
       const campeonatos = data;
-
-      createCards(campeonatos);
+      createCards(campeonatos)
+    })
+    .catch(error => {
+      createErrorCard('league')
     });
 });
 
